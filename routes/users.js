57f61f11ec66bfeb5a6e2f7f050684d35637d2ab
y-kishida <k.yoshikazu1221@gmail.com>
2020-12-04 22:47:11 +0900

    login function

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// ログイン　GET,POST
+router.get('/login', (req, res, next) => {
+  var data = {
+    title: 'User/Login',
+    content: '名前とパスワードを入力ください。'
+  }
+  res.render('users/login', data);
+});
+
+router.post('/login', (req, res, next) => {
+  db.User.findOne({
+    where: {
+      name: req.body.name,
+      pass: req.body.pass,
+    }
+  }).then(usr=>{
+    if (usr != null) {
+      req.session.login = usr;
+      let back = req.session.back;
+      if (back == null) {
+        back = '/';
+      }
+      res.redirect(back);
+    } else {
+      var data = {
+        title: 'Users/Login',
+        content: '名前かパスワードに問題があります。再度入力ください。'
+      }
+      res.render('users/login', data);
+    }
+  })
+});
+
 module.exports = router;
